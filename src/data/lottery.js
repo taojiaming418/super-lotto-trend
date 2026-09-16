f9505c7fe425b9e8f6ba49793f932dc6972d5c91
taojiaming418 <taojm0418@163.com>
2026-09-16 22:00:08 +0800

    auto: update lottery data to 26106

--- src/data/lottery.js
+++ src/data/lottery.js
@@ -1,6 +1,7 @@
 // 大乐透历史开奖数据（自动从 163 彩票更新）
-// 共 991 期，最新: 26105 (2026-09-14)
+// 共 992 期，最新: 26106 (2026-09-16)
 export const lotteryData = [
+  { period: 26106, date: '2026-09-16', day: '三', frontNumbers: [14, 17, 21, 25, 28], backNumbers: [4, 6] },
   { period: 26105, date: '2026-09-14', day: '一', frontNumbers: [10, 14, 30, 33, 34], backNumbers: [9, 12] },
   { period: 26104, date: '2026-09-12', day: '六', frontNumbers: [1, 6, 14, 15, 26], backNumbers: [8, 9] },
   { period: 26103, date: '2026-09-09', day: '三', frontNumbers: [3, 16, 17, 32, 35], backNumbers: [2, 12] },
